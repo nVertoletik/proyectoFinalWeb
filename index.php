15c91bee335dbfa9665ae867f56e3f88732f16fe
Néstor "Vertoletik <alvarado.nestor@yandex.com>
2018-04-21 14:13:54 -0500

    Formateé el código para que sea entendible

---
+++ index.php
@@ -0,0 +1,336 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Proyecto final</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link type="text/css" rel="stylesheet" href="https://static.xx.fbcdn.net/rsrc.php/v3/yy/l/0,cross/EkDCiSdHSpG.css" data-bootloader-hash="1xSPS" data-permanent="1" crossorigin="anonymous" />
+<link type="text/css" rel="stylesheet" href="https://static.xx.fbcdn.net/rsrc.php/v3/yZ/l/0,cross/wsIDFpsneA2.css" data-bootloader-hash="eNtIv" data-permanent="1" crossorigin="anonymous" />
+<link type="text/css" rel="stylesheet" href="https://static.xx.fbcdn.net/rsrc.php/v3/ym/l/0,cross/z-wnmRmoMKW.css" data-bootloader-hash="67Qjk" data-permanent="1" crossorigin="anonymous" />
+<link type="text/css" rel="stylesheet" href="https://static.xx.fbcdn.net/rsrc.php/v3/yn/l/0,cross/HbRvWLo2Xj2.css" data-bootloader-hash="44wiD" crossorigin="anonymous" />
+<link type="text/css" rel="stylesheet" href="https://static.xx.fbcdn.net/rsrc.php/v3/yP/l/0,cross/qBsySn9QzMf.css" data-bootloader-hash="67cKT" data-permanent="1" crossorigin="anonymous" />
+<link type="text/css" rel="stylesheet" href="https://static.xx.fbcdn.net/rsrc.php/v3/y2/l/0,cross/lZ86cv9aR90.css" data-bootloader-hash="f+J2L" crossorigin="anonymous" />
+    <script src="main.js"></script>
+</head>
+<body>
+<h1>Prestashop recargado</h1>
+</body>
+<table>
+        <tbody>
+            <tr>
+                <td class="html7magic">
+                    <label for="email">Usuario</label>
+                </td>
+                <td class="html7magic">
+                    <label for="pass">Contraseña</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input class="inputtext" name="usuario" id="usuario" tabindex="1" data-testid="royal_user" type="text">
+                </td>
+                <td>
+                    <input class="inputtext" name="pass" id="pass" tabindex="2" data-testid="royal_pass" type="password">
+                </td>
+                <td>
+                    <label class="uiButton uiButtonConfirm" id="loginbutton" for="u_0_2">
+                        <input value="Iniciar sesión" tabindex="4" data-testid="royal_login_button" id="u_0_2" type="submit">
+                    </label>
+                </td>
+            </tr>
+        </tbody>
+        </table>
+        <div>
+                <div class="mtm mbs _2_68">Registro
+        
+                    </div>
+            <div id="fullname_field" class="_1ixn">
+                <div class="clearfix _58mh">
+                    <div class="mbm _1iy_ _a4y _3-90 lfloat _ohe">
+                        <div class="_5dbb" id="u_0_c">
+                            <input class="inputtext _58mg _5dba _2ph-" data-type="text" name="firstname" aria-required="1" placeholder="First name" aria-label="First name" id="u_0_d" type="text">
+                            <i class="_5dbc img sp_YWFhuth6uiX sx_fe3591">
+        
+                            </i>
+                            <i class="_5dbd img sp_YWFhuth6uiX sx_3e601f">
+        
+                            </i>
+                            <div class="_1pc_">
+        
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mbm _1iy_ _a4y rfloat _ohf">
+                        <div class="_5dbb" id="u_0_e">
+                            <input class="inputtext _58mg _5dba _2ph-" data-type="text" name="lastname" aria-required="1" placeholder="Last name" aria-label="Last name" id="u_0_f" type="text">
+                            <i class="_5dbc img sp_YWFhuth6uiX sx_fe3591">
+        
+                            </i>
+                            <i class="_5dbd img sp_YWFhuth6uiX sx_3e601f">
+        
+                            </i>
+                            <div class="_1pc_">
+        
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="_1pc_" id="fullname_error_msg">
+        
+                </div>
+            </div>
+            <div class="mbm _a4y" id="u_0_g">
+                <div class="_5dbb" id="u_0_h">
+                    <input class="inputtext _58mg _5dba _2ph-" data-type="text" name="reg_email__" aria-required="1" placeholder="Mobile number or email" aria-label="Mobile number or email" id="u_0_i" type="text">
+                    <i class="_5dbc img sp_YWFhuth6uiX sx_fe3591">
+        
+                    </i>
+                    <i class="_5dbd img sp_YWFhuth6uiX sx_3e601f">
+        
+                    </i>
+                    <div class="_1pc_">
+        
+                    </div>
+                </div>
+            </div>
+            <div class="hidden_elem" id="u_0_j">
+                <div class="mbm _a4y">
+                    <div class="_5dbb" id="u_0_k">
+                        <input class="inputtext _58mg _5dba _2ph-" data-type="text" name="reg_email_confirmation__" aria-required="1" placeholder="Re-enter email" aria-label="Re-enter email" id="u_0_l" type="text">
+                        <i class="_5dbc img sp_YWFhuth6uiX sx_fe3591">
+        
+                        </i>
+                        <i class="_5dbd img sp_YWFhuth6uiX sx_3e601f">
+        
+                        </i>
+                        <div class="_1pc_">
+        
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mbm _br- _a4y hidden_elem" id="u_0_m">
+                <input class="inputtext _58mg _5dba _2ph-" data-type="text" name="reg_second_contactpoint__" placeholder="Mobile Number" aria-label="Mobile Number" id="u_0_n" type="text">
+            </div>
+            <div class="mbm _br- _a4y" id="password_field">
+                <div class="_5dbb" id="u_0_o">
+                    <input class="inputtext _58mg _5dba _2ph-" data-type="password" autocomplete="new-password" name="reg_passwd__" aria-required="1" placeholder="New password" aria-label="New password" id="u_0_p" type="password">
+                    <i class="_5dbc img sp_YWFhuth6uiX sx_fe3591">
+        
+                    </i>
+                    <i class="_5dbd img sp_YWFhuth6uiX sx_3e601f">
+        
+                    </i>
+                    <div class="_1pc_">
+        
+                    </div>
+                </div>
+            </div>
+            <div class="_58mq _5dbb" id="u_0_q">
+                <div class="mtm mbs _2_68">Birthday
+        
+                </div>
+                <div class="_5k_5">
+                    <span class="_5k_4" data-type="selectors" data-name="birthday_wrapper" id="u_0_r">
+                        <span>
+                            <select aria-label="Month" name="birthday_month" id="month" title="Month" class="_5dba">
+                                <option value="0">Month
+        
+                                </option>
+                                <option value="1">Jan
+        
+                                </option>
+                                <option value="2">Feb
+        
+                                </option>
+                                <option value="3">Mar
+        
+                                </option>
+                                <option value="4" selected="1">Apr
+        
+                                </option>
+                                <option value="5">May
+                                    
+                                </option>
+                                <option value="6">Jun
+        
+                                </option>
+                                <option value="7">Jul
+        
+                                </option>
+                                <option value="8">Aug
+        
+                                </option>
+                                <option value="9">Sep
+        
+                                </option>
+                                <option value="10">Oct
+        
+                                </option>
+                                <option value="11">Nov
+        
+                                </option>
+                                <option value="12">Dec
+        
+                                </option>
+                            </select>
+                            <select aria-label="Day" name="birthday_day" id="day" title="Day" class="_5dba">
+                                <option value="0">Day
+        
+                                </option>
+                                <option value="1">1
+        
+                                </option>
+                                <option value="2">2
+        
+                                </option>
+                                <option value="3">3
+        
+                                </option>
+                                <option value="4">4
+        
+                                </option>
+                                <option value="5">5
+        
+                                </option>
+                                <option value="6">6
+        
+                                </option>
+                                <option value="7">7
+        
+                                </option>
+                                <option value="8">8
+        
+                                </option>
+                                <option value="9">9
+        
+                                </option>
+                                <option value="10">10
+        
+                                </option>
+                                <option value="11">11
+        
+                                </option>
+                                <option value="12">12
+        
+                                </option>
+                                <option value="13">13
+        
+                                </option>
+                                <option value="14">14
+        
+                                </option>
+                                <option value="15">15
+        
+                                </option>
+                                <option value="16">16
+        
+                                </option>
+                                <option value="17">17
+        
+                                </option>
+                                <option value="18">18
+        
+                                </option>
+                                <option value="19">19
+        
+                                </option>
+                                <option value="20">20
+        
+                                </option>
+                                <option value="21" selected="1">21
+        
+                                </option>
+                                <option value="22">22
+        
+                                </option>
+                                <option value="23">23
+        
+                                </option>
+                                <option value="24">24
+        
+                                </option>
+                                <option value="25">25
+        
+                                </option>
+                                <option value="26">26
+        
+                                </option>
+                                <option value="27">27
+        
+                                </option>
+                                <option value="28">28
+        
+                                </option>
+                                <option value="29">29
+        
+                                </option>
+                                <option value="30">30
+        
+                                </option>
+                                <option value="31">31
+        
+                                </option>
+                            </select>
+                            <select aria-label="Year" name="birthday_year" id="year" title="Year" class="_5dba">
+                                <option value="0">Year</option>
+                                <option value="2018">2018</option>
+                                <option value="2017">2017</option>
+                                <option value="2016">2016</option>
+                                <option value="2015">2015</option>
+                                <option value="2014">2014</option>
+                                </select>
+                            </span>
+                        </span>
+                        <a class="_58ms mlm" id="birthday-help" href="#" title="Click for more information" role="button">Why do I need to provide my birthday?</a>
+                        <i class="_5dbc _5k_6 img sp_YWFhuth6uiX sx_fe3591">
+        
+                        </i>
+                        <i class="_5dbd _5k_7 img sp_YWFhuth6uiX sx_3e601f">
+        
+                        </i>
+                        <div class="_1pc_">
+        
+                        </div>
+                    </div>
+                </div>
+                <div class="mtm _5wa2 _5dbb" id="u_0_s">
+                    <span class="_5k_3" data-type="radio" data-name="gender_wrapper" id="u_0_t">
+                        <span class="_5k_2 _5dba">
+                            <input name="sex" value="1" id="u_0_a" type="radio">
+                            <label class="_58mt" for="u_0_a">Female</label>
+                        </span>
+                        <span class="_5k_2 _5dba">
+                            <input name="sex" value="2" id="u_0_b" type="radio">
+                            <label class="_58mt" for="u_0_b">Male</label>
+                        </span>
+                    </span>
+                    <i class="_5dbc _5k_6 img sp_YWFhuth6uiX sx_fe3591">
+        
+                    </i>
+                    <i class="_5dbd _5k_7 img sp_YWFhuth6uiX sx_3e601f">
+        
+                    </i>
+                    <div class="_1pc_">
+        
+                    </div>
+                </div>
+                <div class="_58mu" data-nocookies="1" id="u_0_u">
+                    <p class="_58mv">By clicking Sign Up, you agree to our
+                        <a href="/legal/terms/update" id="terms-link" target="_blank" rel="nofollow">Terms</a>
+                        , <a href="/about/privacy/update" id="privacy-link" target="_blank" rel="nofollow">Data Policy</a>
+                         and <a href="/policies/cookies/" id="cookie-use-link" target="_blank" rel="nofollow">Cookies Policy</a>
+                         . You may receive SMS Notifications from us and can opt out any time.
+                        </p>
+                    </div>
+                    <div class="clearfix">
+                        <button type="submit" class="_6j mvm _6wk _6wl _58mi _3ma _6o _6v" name="websubmit" id="u_0_v">Sign Up
+        
+                        </button>
+                        
+                    </div>
+        </div>
+</html>
