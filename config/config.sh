11039398c2bfd8d093bdf40193b924ea990a31a1
Néstor "Vertoletik <alvarado.nestor@yandex.com>
2018-05-28 20:26:08 -0500

    Modifiqué el script de instalación

--- config/config.sh
+++ config/config.sh
@@ -1,4 +1,5 @@
-mv prestashop.com.conf /etc/apache2/sites-available/
-a2ensite /etc/apache2/sites-available/
-echo "127.0.0.1     www.prestashop.com" >>/etc/hosts
-service apache2 restart
+#! /bin/shell
+mv prestashop.com.conf /etc/apache2/sites-available/;
+a2ensite /etc/apache2/sites-available/;
+echo "127.0.0.1     www.prestashop.com" >>/etc/hosts;
+service apache2 restart;
