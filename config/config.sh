86da9472238e2cbaf5a1a95d6c312ebf46fc5039
Nestor 'Hackerman <alvarado.nestor@yandex.com>
2018-05-29 11:20:09 +0000

    Agregue los cert

--- config/config.sh
+++ config/config.sh
@@ -2,4 +2,8 @@
 mv prestashop.com.conf /etc/apache2/sites-available/;
 a2ensite /etc/apache2/sites-available/;
 echo "127.0.0.1     www.prestashop.com" >>/etc/hosts;
+mv config/llave-prestashop.key /etc/ssl/private/
+mv certificado-prestashop.crt /etc/ssl/certs/
+chmod 644 /etc/ssl/certs/certificado-3pnalvarado.crt
+chmod 644 /etc/ssl/private/llave-3pnalvarado.key
 service apache2 restart;
